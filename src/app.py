736c3b286bf9aa40c1f2804a43e9f850e64cf43b
Alexey <bazooka2008@inbox.ru>
2026-04-26 23:14:06 +0300

    aaa

---
+++ src/app.py
@@ -0,0 +1,29 @@
+import logging
+
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
+from src.api import main_router
+from src.settings import settings
+
+
+class App(FastAPI):
+    def __init__(self):
+        super().__init__(title="Matyan", lifespan=App.lifespan, debug=True)
+        super().include_router(main_router)
+        super().add_middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+
+    @staticmethod
+    @asynccontextmanager
+    async def lifespan(_app: FastAPI):
+        logging.info("All resources have been successfully initialized")
+
+        yield
+
+        logging.info("All resources have been successfully closed")
